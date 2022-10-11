d6c0a7222dc873123d7eebced3b9998f961e4b6c
Dominic London <londonrdominic@gmail.com>
2022-10-10 21:08:08 -0700

    Initial Git upload

---
+++ Yellow Boi Unity/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        // for testing/debugging
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
