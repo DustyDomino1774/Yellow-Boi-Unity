ecc4f98aa52b220f1bb91338f1707564a2ee3a8d
Dominic London <londonrdominic@gmail.com>
2022-11-13 23:07:22 -0800

    Added red and green ghost prefab

--- Yellow Boi Unity/Assets/Scripts/PlayerPrey.cs
+++ Yellow Boi Unity/Assets/Scripts/PlayerPrey.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class PlayerPrey : MonoBehaviour
 {
     public Movement movement { get; private set; }
+    public AnimatedSprite death;
+    public SpriteRenderer spriteRenderer { get; private set; }
+    public new Collider2D collider { get; private set; }
 
     private void Awake()
     {
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+        this.collider = GetComponent<Collider2D>();
         this.movement = GetComponent<Movement>();
     }
     // Update is called once per frame
@@ -34,4 +39,27 @@ public class PlayerPrey : MonoBehaviour
         float rotAngle = Mathf.Atan2(this.movement.direction.y, this.movement.direction.x);
         this.transform.rotation = Quaternion.AngleAxis(rotAngle * Mathf.Rad2Deg, Vector3.forward); // rads so convert to degree
     }
+
+    public void ResetState()
+    {
+        enabled = true;
+        spriteRenderer.enabled = true;
+        collider.enabled = true;
+        death.enabled = false;
+        death.spriteRenderer.enabled = false;
+        movement.ResetState();
+        gameObject.SetActive(true);
+    }
+
+    public void DeathSequence()
+    {
+        enabled = false;
+        spriteRenderer.enabled = false;
+        collider.enabled = false;
+        movement.enabled = false;
+        death.enabled = true;
+        death.spriteRenderer.enabled = true;
+        death.Reset();
+    }
+
 }
